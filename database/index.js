a514b38e12fec2b35c5a2c91f3d90ab7c70916a8
bhagas koro <tump.store@gmail.com>
2016-06-13 04:50:37 +0700

    ubah koneksi mysql

--- database/index.js
+++ database/index.js
@@ -7,9 +7,9 @@
  var mysql      = require('mysql');
 
 module.exports.connection = mysql.createConnection({
-  host     : '128.199.196.228',
-  user     : 'root',
-  port	   : '3306',
-  password : 'sawunggaling26a',
-  database : 'hrvplanner_ciptakarya_2016'
+  host     : '',
+  user     : '',
+  port	   : '',
+  password : '',
+  database : ''
 });
